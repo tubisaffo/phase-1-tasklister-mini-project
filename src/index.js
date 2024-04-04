071803938d42d7227e12dea3663c845b1ce7be8a
tubisaffo <tubisaffo@gmail.com>
2024-04-04 22:02:20 +0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskList = document.querySelector("#tasks");
+  const createNewTask = event => {
+    event.preventDefault();
+    const newTask = document.createElement("li")
+    newTask.innerText = document.getElementById("new-task-description").value
+    taskList.appendChild(newTask);
+    event.target.reset();
+
+    
+  }
+  document.querySelector("#create-task-form").addEventListener("submit", createNewTask);
+})
